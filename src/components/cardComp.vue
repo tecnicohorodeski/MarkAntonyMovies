9c7f361cfb47fd95993e6e09bef13703755cf022
geossophia <geovana.horodeski06@gmail.com>
2022-09-14 09:13:40 -0300

    gsh

---
+++ src/components/cardComp.vue
@@ -0,0 +1,13 @@
+<script>
+export default {};
+</script>
+<template>
+  <div class="cards">
+    <img src="src/assets/img/undraw_home_cinema_l7yl.png" alt="" />
+    <h3>lorem lorem</h3>
+    <h4>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto neque
+      cumque dicta debitis
+    </h4>
+  </div>
+</template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,17 +1,27 @@
-<script setup></script>
+<script setup>
+import CardComp from "../components/cardComp.vue";
+</script>
 
 <template>
   <div class="fundo">
     <div class="textin">
       <div class="infos">
-        <h2>Lorem, ipsum dolor sit amet consectetur adipisicing</h2>
+        <h2>
+          Lorem, ipsum dolor sit amet consectetur <strong>adipisicing</strong>
+        </h2>
         <p>
           Lorem ipsum dolor sit amet consectetur, adipisicing elit. Qui omnis,
           excepturi assumenda porro nulla iusto. <br />Dolorem sit repudiandae
           impedit minima dignissimos cupiditate voluptatibus optio perspiciatis
           quo deleniti, similique quam nam.
         </p>
+        <button>VER MAIS</button>
       </div>
     </div>
+  </div>
+  <div>
+    <CardComp />
+    <CardComp />
+    <CardComp />
   </div>
 </template>
