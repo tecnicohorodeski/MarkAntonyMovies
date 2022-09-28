9e4018121ec706d985f0e7e0cf9f0871b21f913e
geossophia <geovana.horodeski06@gmail.com>
2022-09-28 09:19:02 -0300

    GSH

---
+++ src/components/section3Comp.vue
@@ -0,0 +1,26 @@
+<script>
+export default {};
+</script>
+<template>
+  <div class="filmes-filtros">
+    <h2 class="filmes">filmes</h2>
+    <nav class="nav-filtros">
+      <div class="filtros">
+        <div class="pesquisadiv">
+          <label for="pesquisa">pesquise</label>
+          <input type="text" id="pesquisa" />
+        </div>
+        <div class="selects">
+          <div class="casa">
+            <label for="">categoria</label>
+            <select name="" id=""></select>
+          </div>
+          <div class="casa">
+            <label for="">saga</label>
+            <select name="" id=""></select>
+          </div>
+        </div>
+      </div>
+    </nav>
+  </div>
+</template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,12 +1,14 @@
 <script>
 import CardComp from "../components/cardComp.vue";
 import imgComp from "../components/imgComp.vue";
+import Section3Comp from "../components/section3Comp.vue";
 export default {
   name: "app",
   components: {
     CardComp,
     imgComp,
-  },
+    Section3Comp,
+},
 };
 </script>
 
@@ -41,24 +43,6 @@ export default {
     <i class="bx bx-chevrons-down"></i>
   </div>
   <div class="section3">
-    <h2 class="filmes">filmes</h2>
-    <nav class="nav-filtros">
-      <div class="filtros">
-        <div class="pesquisadiv">
-          <label for="pesquisa">pesquise</label>
-          <input type="text" id="pesquisa" />
-        </div>
-        <div class="selects">
-          <div class="casa">
-            <label for="">categoria</label>
-            <select name="" id=""></select>
-          </div>
-          <div class="casa">
-            <label for="">saga</label>
-            <select name="" id=""></select>
-          </div>
-        </div>
-      </div>
-    </nav>
+    <Section3Comp />
   </div>
 </template>
